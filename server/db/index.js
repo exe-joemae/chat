55b75207857efa0535a4a550611ec67a1b8ccdb5
subete_Teigino_sei <maji4maro@gmail.com>
2026-06-19 21:18:35 +0900

    Create index.js

---
+++ server/db/index.js
@@ -0,0 +1,8 @@
+const { Pool } = require('pg');
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString, ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false });
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  pool
+};
